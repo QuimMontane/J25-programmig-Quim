78cdd722dda3d319b2753bd6ef4179cd625b4933
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2023-11-21 12:39:44 +0100

    Create ProblemSolver3.ino

---
+++ Arduino/ProblemSolver/ProblemSolver3/ProblemSolver3./ProblemSolver3.ino
@@ -0,0 +1,19 @@
+void setup() {     //Problems divisible by both 4 and 6
+  Serial.begin(9600);
+
+  for (int n = 1; n <= 100; n++) {
+    if (n % 4 == 0 && n % 6 == 0) {
+      resolverProblemaDibujo(n);
+    }
+  }
+}
+
+void loop() {
+  // This function will be called repeatedly after the setup function has executed.
+  // However, the loop function is empty in this code.
+}
+
+void resolverProblemaDibujo(int n) {
+  Serial.print("Resolviendo problema ");
+  Serial.println(n);
+}
