586786d8778f9e31e2e5c1e05ad9439755198e20
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2023-12-14 18:22:22 +0100

    Create Song2.ino

---
+++ Arduino/Song/Song 2/Song2.ino
@@ -0,0 +1,225 @@
+//Los peces en el rio (simplificated) melody
+
+void setup() {
+  pinMode (9, OUTPUT);
+
+// Do = c
+// Re = d
+// Mi = e
+// Fa = f
+// Sol = g
+// La = a
+// Si = b
+
+int c=523;
+int d=587;
+int e=659;
+int f=698;
+int g=783;
+int a=880;
+int b=987;
+
+// 4=y 
+// 2=x
+// 1=z
+// 0.5=w
+
+int y=60000/440;
+int x=60000/220;
+int z=60000/110;
+int w=60000/55;
+
+//do 1 -
+//do 0.5 -
+//do 1 -
+//do 1 -
+//si 0.5 -
+//la 1 -
+//si 1 -
+//mi 2-
+//re 1 -
+//re 0.5 -
+//re 1 -
+//re 1 -
+//do 0.5 -
+//si 1 -
+//do 1 -
+//si 1 -
+//la 2 - 
+//do 1 -
+//do 1 -
+//do 1 -
+//do 1 -
+//si 1 -
+//la 1 -
+//si 1 -
+//sol 1 -
+//mi 2 -
+//re 1 -
+//re 0.5 -
+//re 1 -
+//re 1 -
+//do 0.5 -
+//la 1 -
+//do 1 -
+//si 1 -
+//la 1 -
+//la 0.5 -
+//si 1 -
+//do 0.5 -
+//do 0.5 -
+//do 0.5 -
+//do 1 -
+//do 1 -
+//do 0.5 -
+//do 0.5 -
+//re 0.5 -
+//do 0.5 -
+//re 0.5 -
+//do 1 -
+//si 1 -
+//si 0.5 -
+//la 1 -
+//sol 0.5 -
+//la 0.5 -
+//si 0.5 -
+//do 1 -
+//la 2 -
+//do 1 -
+//do 0.5 -
+//do 1 -
+//do 1 -
+//do 0.5 -
+//do 1 -
+
+tone(9,c,z);
+  delay(z+50);       
+tone(9,c,w);
+  delay(w+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,b,w);
+  delay(w+50);
+tone(9,a,z);
+  delay(z+50);
+tone(9,b,z);
+  delay(z+50);
+tone(9,e,x);
+  delay(x+50);
+tone(9,d,z);
+  delay(z+50);
+tone(9,d,w);
+  delay(w+50);
+tone(9,d,z);
+  delay(z+50);
+tone(9,d,z);
+  delay(z+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,b,z);
+  delay(z+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,b,z);
+  delay(z+50);
+tone(9,a,x);
+  delay(x+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,b,z);
+  delay(z+50);
+tone(9,a,z);
+  delay(z+50);
+tone(9,b,z);
+  delay(z+50);
+tone(9,g,z);
+  delay(z+50);
+tone(9,e,x);
+  delay(x+50);
+tone(9,d,z);
+  delay(z+50);
+tone(9,d,w);
+  delay(w+50);
+tone(9,d,z);
+  delay(z+50);
+tone(9,d,z);
+  delay(z+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,a,z);
+  delay(z+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,b,z);
+  delay(z+50);
+tone(9,a,z);
+  delay(z+50);
+tone(9,a,w);
+  delay(w+50);
+tone(9,b,z);
+  delay(z+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,d,w);
+  delay(w+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,d,w);
+  delay(w+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,b,z);
+  delay(z+50);
+tone(9,b,w);
+  delay(w+50);
+tone(9,a,z);
+  delay(z+50);
+tone(9,g,w);
+  delay(w+50);
+tone(9,a,w);
+  delay(w+50);
+tone(9,b,w);
+  delay(w+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,a,x);
+  delay(x+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,z);
+  delay(z+50);
+tone(9,c,w);
+  delay(w+50);
+tone(9,c,z);
+  delay(z+50);
+  
+
+}
+
+void loop() {
+
+}
