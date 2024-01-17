c96b4933b73fa015f08d06392e1cddbeb0dc50ef
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-01-17 10:36:31 +0100

    Add files via upload

---
+++ Arduino/HomeworkDefiningAFunction-QuimMontane.ino
@@ -0,0 +1,8 @@
+//ex2 
+void startWithQuixote(int_string);
+void inverseNumber(int n);
+bool isItAGoodDay();
+double volumeCilinder(double radius, double height);
+bool isThereAnyBeetlejuice(String x);
+void notOver5(int n);
+void signLullaby();
