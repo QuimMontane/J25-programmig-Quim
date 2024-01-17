c085cc8a882f07a456ea7f4d578bf40fb85b0aca
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-01-17 10:39:00 +0100

    Update HomeworkDefiningAFunction-QuimMontane.ino

--- Arduino/HomeworkDefiningAFunction/HomeworkDefiningAFunction-QuimMontane.ino
+++ Arduino/HomeworkDefiningAFunction/HomeworkDefiningAFunction-QuimMontane.ino
@@ -1,8 +1,21 @@
 //ex2 
+Define the function startWithQuixote that if you send an String is going to return an string that is the same but it will start with "En un lugar de la Mancha… ".
 void startWithQuixote(int_string);
+
+Define a function that is going to output on a screen (or through serial) the inverse (1/x) of the integer number that you send to it. The name of the function is inverseNumber
 void inverseNumber(int n);
+
+Define a function in C++ that is going to return true if it’s a “good day” and false otherwise. The name of the function is “isItAGoodDay”.
 bool isItAGoodDay();
+
+Define a function in C++ that is going to return you the volume of a cilinder given it’s radius and it’s height. Numbers can be decimals. The name of the function is volumeCilinder.
 double volumeCilinder(double radius, double height);
+
+Define a function called “isThereAny Beetlejuice” that is going to analyse a string given as a parameter and is going to return true if the name “Beetlejuice” is part of the string and false otherwise
 bool isThereAnyBeetlejuice(String x);
+
+Define a function that is called notOver5 that given an integer is going to return the same integer if it’s not over 5 and 5 if it’s equal or bigger than five.
 void notOver5(int n);
+
+Define a function called singLullaby that is going to tone a lullaby when called.
 void signLullaby();
