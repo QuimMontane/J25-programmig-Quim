886a5ea30e3d5f3e44c57cf235c688da76721bfd
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-01-18 08:31:46 +0100

    Update HomeworkDefiningAFunction-QuimMontane.ino

--- Arduino/HomeworkDefiningAFunction/HomeworkDefiningAFunction-QuimMontane.ino
+++ Arduino/HomeworkDefiningAFunction/HomeworkDefiningAFunction-QuimMontane.ino
@@ -1,6 +1,6 @@
 //ex2 
 Define the function startWithQuixote that if you send an String is going to return an string that is the same but it will start with "En un lugar de la Mancha… ".
-void startWithQuixote(int_string);
+string startWithQuixote(String x);
 
 Define a function that is going to output on a screen (or through serial) the inverse (1/x) of the integer number that you send to it. The name of the function is inverseNumber
 void inverseNumber(int n);
@@ -15,7 +15,7 @@ Define a function called “isThereAny Beetlejuice” that is going to analyse a
 bool isThereAnyBeetlejuice(String x);
 
 Define a function that is called notOver5 that given an integer is going to return the same integer if it’s not over 5 and 5 if it’s equal or bigger than five.
-void notOver5(int n);
+int notOver5(int n);
 
 Define a function called singLullaby that is going to tone a lullaby when called.
 void signLullaby();
