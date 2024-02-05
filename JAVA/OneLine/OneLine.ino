6deea5ffb83e098378b4246200042f63dfb5fdec
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-02-05 09:33:43 +0100

    Create OneLine.ino

---
+++ JAVA/OneLine/OneLine.ino
@@ -0,0 +1,17 @@
+public class Main {     
+   public static void main(String[] args) {
+      int number1 = 98; 
+      int number2 = 5;  
+      int sum, difference, product, quotient, remainder;  
+      
+      sum = number1 + number2;
+      difference = number1 - number2;
+      product = number1 * number2;
+      quotient = number1 / number2;
+      remainder = number1 % number2;
+  
+      
+      System.out.print("The sum, difference, product, quotient and remainder of " + number1 + " and " + number2 + " are " + sum + ", " + difference + ", " + product + ", " + quotient + ", and " + remainder); 
+      
+   }
+   }
