b24f2cff043c87f14a1c1513294da35fb3a8ea36
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-02-05 08:40:34 +0100

    Create Triangle_area.ino

---
+++ JAVA/Triangle/Triangle_area.ino
@@ -0,0 +1,15 @@
+public class Main {  
+   public static void main(String[] args) {
+     
+      double area, base, height;
+      
+      base = 12.3465;
+      height = 15.5423;
+      
+      
+      area = (base * height) / 2;
+   
+      System.out.print("The area is ");  
+      System.out.println(area);          
+   }
+}
