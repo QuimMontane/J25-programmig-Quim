53d1846fb6c2ed0b2aa07c052894bebf90a5bd59
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-02-05 09:50:39 +0100

    Create Sum_ofTwoNumbers.ino

---
+++ JAVA/SUM/Sum_ofTwoNumbers/Sum_ofTwoNumbers.ino
@@ -0,0 +1,9 @@
+  public class Main {  
+   public static void main(String[] args) {
+      double number1 = 99;
+      double number2 = 4;
+      double sum = 0;
+      sum = (number1*31)+(number2*17);
+      System.out.println(sum);
+   }
+}
