901cf1495911d2e7e73d071b87af455d5c33d368
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-02-05 09:51:59 +0100

    Create Sum_AddingAnotherNumber.ino

---
+++ JAVA/SUM/Sum_AddingAnotherNumber/Sum_AddingAnotherNumber.ino
@@ -0,0 +1,8 @@
+ public class Main {  
+   public static void main(String[] args) {
+      double number1 = 99;
+      double number2 = 4;
+      int number3 = 77;
+      double sum = 0;
+      sum = (number1*31)+(number2*17)+number3;
+      System.out.println(sum);
