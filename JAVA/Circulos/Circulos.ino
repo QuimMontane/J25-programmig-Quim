c18dceb9000c4180cb4d034d49fcfcd44d91e235
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-03-07 08:16:45 +0100

    Update Circulos.ino

--- JAVA/Circulos/Circulos.ino
+++ JAVA/Circulos/Circulos.ino
@@ -2,8 +2,8 @@ Circle circle1 = new Circle();
 
 Circle c2 = new Circle(5.0);
 
-Circle c3 = new Circle(10.0, "blue");
+Circle c3 = new Circle(10.0, "green");
 
-Circle colinRobinson = new Circle(-2.0, "bluish");
+Circle colinRobinson = new Circle(-2.0, "greenish");
 
-Circle circle5 = new Circle(1.0, "green");
+Circle circle5 = new Circle(1.0, "blue");
