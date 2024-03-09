b5f653e021f55df9a1dbfdd6a7edbcfef94fa1d2
QuimMontane <144990948+QuimMontane@users.noreply.github.com>
2024-03-09 12:33:06 +0100

    Create Objetos en diferentes contextos.ino

---
+++ JAVA/Objetos en diferentes contextos/Objetos en diferentes contextos.ino
@@ -0,0 +1,28 @@
+Water
+
+1) Aquaservice:
+	Water Delivery
+	- String: DeliveryID
+	- String: CustomerName
+	- String: DeliveryAdress
+	- int: BottleSize
+	- String: DeliveryDate
+	- Bool: PaymentStatus
+	
+2) Supermarket:
+	Water Bottle	
+	- String: Brand
+	- int: Volume
+	- double: Price
+	- String: ExpirationDate
+	- int: QualityInStock
+	- String: Supplier
+	
+3) Water Factory:
+	Water Production
+	- String: ProductionID
+	- String: ProductionDate
+	- int: VolumeProduced
+	- String: Quality
+	- String: PackagingType
+	- String: ProductionLine
